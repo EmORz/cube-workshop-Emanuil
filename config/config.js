78ef5f560a26b321cbae65503bf2daca5dc6c58a
Emo <eiv.razgrad@gmail.com>
2020-06-06 14:10:02 +0300

    feat: update template

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     development: {
 
-        port: 'mongodb://localhost:27017/CubicEmo1'
-       // port: process.env.PORT || 3000
+        //port: 'mongodb://localhost:27017/CubicEmo1'
+         port: process.env.PORT || 3000
         //connectionString: 'mongodb://localhost:27017/SoftUniWikiEmo',
     },
     production: {}
