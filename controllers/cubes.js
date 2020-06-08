ef13e823397c444660bddaa6708868e321fbf7df
Emo <eiv.razgrad@gmail.com>
2020-06-08 17:09:37 +0300

    remove bug-conflict in cube.js

--- controllers/cubes.js
+++ controllers/cubes.js
@@ -5,10 +5,7 @@ const getAllCubes = async () => {
   return cubes;
 };
 
-const getAllCubesSearch = async (search) => {
-  const cubes = await Cube.find(x => x.name ===search);
-  return cubes;
-};
+
 
 
 const getCube = async (id) => {
@@ -21,17 +18,9 @@ const getCubeWithAccessories = async (id) => {
   return cube;
 };
 
-<<<<<<< Updated upstream
-const updateCube = async (cubeId, accessoryId) =>{
-=======
-const getCubeWithAccessories = async (id) => {
-  const cube = await Cube.findById(id).populate('accessories').lean();
-console.log(cube)
-  return cube;
-};
 const updateCube = async (cubeId, accessoryId) =>{
 
->>>>>>> Stashed changes
+
   await Cube.findByIdAndUpdate(cubeId, {
     $addToSet:{
       accessories: [accessoryId]
@@ -42,6 +31,6 @@ module.exports = {
   getAllCubes,
   getCube,
   updateCube,
-  getCubeWithAccessories,
-  getAllCubesSearch
-};
+  getCubeWithAccessories
+ 
+}
