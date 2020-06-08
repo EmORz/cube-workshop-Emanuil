32a0a8fbaa60f79a13a5320d8d45172e6a9d0d7f
Emo <eiv.razgrad@gmail.com>
2020-06-08 11:45:33 +0300

    feat/clean-code-add-dotenv

--- config/config.js
+++ config/config.js
@@ -1,9 +1,14 @@
+require('dotenv').config()
+
+
+
 module.exports = {
     development: {
 
         //port: 'mongodb://localhost:27017/CubicEmo1'
-         port: process.env.PORT || 3000,
-         databaseUrl: `mongodb+srv://user:123@softuniemo-q41wx.mongodb.net/cubicle?retryWrites=true&w=majority`
+         port: process.env.PORT,
+         
+         databaseUrl: `mongodb+srv://user:${process.env.PASS}@softuniemo-q41wx.mongodb.net/cubicle?retryWrites=true&w=majority`
         //connectionString: 'mongodb://localhost:27017/SoftUniWikiEmo',
     },
     production: {}
