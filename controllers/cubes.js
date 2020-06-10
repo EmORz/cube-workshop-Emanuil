90332fb120633a14652d3b27fa2e235d954c319e
Emo <eiv.razgrad@gmail.com>
2020-06-10 10:48:33 +0300

    feat/add-search-function

--- controllers/cubes.js
+++ controllers/cubes.js
@@ -5,32 +5,59 @@ const getAllCubes = async () => {
   return cubes;
 };
 
-
-
-
 const getCube = async (id) => {
   const cube = await Cube.findById(id).lean();
   return cube;
 };
 
 const getCubeWithAccessories = async (id) => {
-  const cube = await Cube.findById(id).populate('accessories').lean();
+  const cube = await Cube.findById(id).populate("accessories").lean();
   return cube;
 };
 
-const updateCube = async (cubeId, accessoryId) =>{
-
-
+const updateCube = async (cubeId, accessoryId) => {
   await Cube.findByIdAndUpdate(cubeId, {
-    $addToSet:{
-      accessories: [accessoryId]
-    }
-  })
-}
+    $addToSet: {
+      accessories: [accessoryId],
+    },
+  });
+};
+
+const searchCubes = async (search, from, to) => {
+  const onlyStringMatches = await Cube.find({
+    $or: [
+      {
+        name: { $regex: search, $options: "i" },
+      },
+      {
+        description: { $regex: search, $options: "i" },
+      },
+    ],
+  }).lean();
+
+  
+  const fullMatches = await Cube.find({
+    $or: [
+      {
+        name: { $regex: search, $options: "i" },
+      },
+      {
+        description: { $regex: search, $options: "i" },
+      },
+    ],
+    difficulty: { $gte: +from, $lte: +to },
+  }).lean();
+
+  if (search && to && from) {
+    return fullMatches;
+  } else {
+    return onlyStringMatches;
+  }
+};
 module.exports = {
   getAllCubes,
   getCube,
   updateCube,
-  getCubeWithAccessories
- 
-}
+  getCubeWithAccessories,
+  searchCubes
+};
