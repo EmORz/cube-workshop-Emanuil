0c002ed7935ed3ac32d1eee28b6c076db00f760f
Emo <eiv.razgrad@gmail.com>
2020-06-16 14:35:27 +0300

    fix/it was fix problems with post request on delete and edit

--- controllers/cubes.js
+++ controllers/cubes.js
@@ -1,6 +1,7 @@
 const Cube = require("../models/cube");
 const { getUserStatus } = require("../controllers/auth");
 const jwt = require("jsonwebtoken");
+
 const secret = "shhhhh";
 
 const getAllCubes = async () => {
@@ -117,35 +118,47 @@ const homePost = async (req, res) => {
   });
 };
 
-const deleteCubePost = (req, res, next) => {
+const deleteCubePost = async (req, res, next) => {
   const id = req.params.id;
-  console.log("Ehoooooo :)");
+ 
 
-  Cube.deleteOne({ _id: id })
-    .then(() => {
-      console.log("Ehoooooo :)");
-      res.redirect("/");
-    })
-    .catch(next);
+  try {
+    const cube = await Cube.findByIdAndDelete({ _id: id })
+      .populate("accessories")
+      .lean();
+    console.log("This is Deleted!Eyap ;)", cube);
+    res.redirect('/')
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-const deleteCubeGet = (req, res, next) => {
+const deleteCubeGet = async (req, res, next) => {
   const id = req.params.id;
   const token = req.cookies["aid"];
   const user = jwt.verify(token, secret);
 
-  console.log(user);
-
-  const userId = user.userID;
-
-  Cube.findOne({ _id: id, creatorId: userId }).then((cube) => {
-    res.render("deleteCube", {
-      title: "Delete Cube",
-      cube,
-      user,
-      isLoggedIn: req.isLoggedIn,
-    });
-  });
+  await Cube.findOne({ _id: id, creatorId: user.userID  })
+    .then((cube) => {
+      const options = [
+        { title: "1 - Very Easy", selected: 1 === cube.difficultyLevel },
+        { title: "2 - Easy", selected: 2 === cube.difficultyLevel },
+        {
+          title: "3 - Medium (Standard 3x3)",
+          selected: 3 === cube.difficultyLevel,
+        },
+        { title: "4 - Intermediate", selected: 4 === cube.difficultyLevel },
+        { title: "5 - Expert", selected: 5 === cube.difficultyLevel },
+        { title: "6 - Hardcore", selected: 6 === cube.difficultyLevel },
+      ];
+      res.render("deleteCube", {
+        cube,
+        options,
+        user,
+        isLoggedIn: req.isLoggedIn,
+      });
+    })
+    .catch(next);
 };
 
 const editCubeGet = async (req, res, next) => {
@@ -154,14 +167,22 @@ const editCubeGet = async (req, res, next) => {
   const token = req.cookies["aid"];
   const user = jwt.verify(token, secret);
 
-  const userId = user.userID;
-
-  await Cube.findById({ _id: cubeId, creatorId: userId })
+  await Cube.findOne({ _id: cubeId, creatorId: user.userID  })
     .then((cube) => {
-     
+      const options = [
+        { title: "1 - Very Easy", selected: 1 === cube.difficultyLevel },
+        { title: "2 - Easy", selected: 2 === cube.difficultyLevel },
+        {
+          title: "3 - Medium (Standard 3x3)",
+          selected: 3 === cube.difficultyLevel,
+        },
+        { title: "4 - Intermediate", selected: 4 === cube.difficultyLevel },
+        { title: "5 - Expert", selected: 5 === cube.difficultyLevel },
+        { title: "6 - Hardcore", selected: 6 === cube.difficultyLevel },
+      ];
       res.render("editCube", {
-        title: "Edit Cube",
         cube,
+        options,
         user,
         isLoggedIn: req.isLoggedIn,
       });
@@ -171,25 +192,18 @@ const editCubeGet = async (req, res, next) => {
 
 const editCubePost = async (req, res, next) => {
   const id = req.params.id;
+
+  const {name = null, description = null, imageUrl = null, difficultyLevel = null} = req.body;
   const token = req.cookies["aid"];
   const user = jwt.verify(token, secret);
 
-  const userId = user.userID;
-  const creatorId = userId;
-  const cube = { ...req.body, id, creatorId };
- 
-
-  console.log(creatorId);
-  // const token = req.cookies["aid"];
-  //const userId = jwt.verify(token, secret).userID;
-
-  // const testUpdate = await Cube.updateOne(
-  //   { _id: id, creatorId: userId },
-  //   { name, description, imageURL, difficulty, decodedObj }
-  // );
-  //console.log(testUpdate);
-
-  res.redirect("/");
+  try {
+    await Cube.updateOne({ _id: id, creatorId: user.userID }, { name, description, imageURL: imageUrl, difficulty: difficultyLevel });
+    res.redirect("/");
+    next();
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
